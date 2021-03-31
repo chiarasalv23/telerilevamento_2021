c891d8af7e6273147448c23363cfb5280d9ebc70
ChiaraXXIII <80899048+chiarasalv23@users.noreply.github.com>
2021-03-31 22:38:22 +0200

    Create R_code_time_series.r

---
+++ R_code_time_series.r
@@ -0,0 +1,10 @@
+#___________________GREENLAND TIME SERIES ANALYSIS______________________
+#Greenland increase of temperature - Data and code from Emanuela Cosma
+#_______________________________________________________________________
+
+#carico pacchetto raster
+library(raster)
+#cambiamo la nostra workin' directory
+setwd("C:/lab/greenland")
+
+
