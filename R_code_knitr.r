a5a5aad57e393309b4805bceda60e10b092bef85
ChiaraXXIII <80899048+chiarasalv23@users.noreply.github.com>
2021-04-16 23:10:11 +0200

    Update R_code_knitr.r

--- R_code_knitr.r
+++ R_code_knitr.r
@@ -1,9 +1,14 @@
 # R_code_knitr.r
-
-setwd("C:/lab/")
+# Creare un report che contenga codice r e grafici!
+setwd("C:/lab/") #dentro lab c'è il codice da usare!
 
 #carico la libreria knitr
 #require(knitr)
 library(knitr)
+
+#utilizziamo la funzine stitch()
+stitch("R_code_greenland.r", template = system.file("misc", "knitr-template.Rnw", package="knitr"))
+#mi genera un file .tex (LaTex)
+
 
 
