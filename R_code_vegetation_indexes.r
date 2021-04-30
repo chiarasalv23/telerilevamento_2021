cbf435c2587002c34e623f29a8c14fc7ccc3997f
ChiaraXXIII <80899048+chiarasalv23@users.noreply.github.com>
2021-04-30 23:48:21 +0200

    Update R_code_vegetation_indexes.r

--- R_code_vegetation_indexes.r
+++ R_code_vegetation_indexes.r
@@ -4,7 +4,7 @@ setwd("C:/lab/")
 
 # DAY 1
 
-#carichiamo i dati defor1.jpg e defor2.jpg
+#carichiamo i dati defor1.jpg e defor2.jpg -> Earth Observatory NASA
 defor1 <- brick("defor1.jpg")
 defor2 <- brick("defor2.jpg")
 
@@ -12,3 +12,65 @@ defor2 <- brick("defor2.jpg")
 par(mfrow = c(2,1))
 plotRGB(defor1, r = 1, g = 2, b = 3, stretch = 'lin')
 plotRGB(defor2, r = 1, g = 2, b = 3, stretch = 'lin')
+
+# DAY 2
+#richiamo i pacchetti che mi servono...
+require(RStoolbox) #per usare alcune funzioni per gli indici di vedetazione
+#setto la working directory...
+#defor1 e defor2 -> sito in cui è stata fatta una forte deforestazione
+#siamo a Rio Peixoto Brazil! 
+#le carichiamo e le riplottiamo...
+
+defor1 #vedo le info del file e le sue bande (vedere come si chiamano)
+# NIR: defor1.1, RED: defor1.2, GREEN: defor1.3
+#CALCOLO DVI per defor1 
+#NIR - RED
+dvi1 <- defor1$defor1.1 - defor1$defor1.2 #per ogni pixel prendiamo il valore NIR meno valore RED
+#in uscita abbiamo una mappa formata da tanti pixel che sono la diffrenza dell'NIR e RED -> DVI difference vegetation index!
+plot(dvi1)
+
+#cambiano la palette di colori
+#colorRampPalette
+cl <- colorRampPalette(c('darkblue', 'yellow', 'red', 'black')) (200)
+plot(dvi1, col = cl, main = 'DV1 at time 1')
+
+#CALCOLO DVI per defor2
+dvi2 <- defor2$defor2.1 - defor2$defor2.2
+plot(dvi2, col = cl, main = 'DVI at time 2') 
+
+#par per vederli insieme... 
+par(mfrow = c(1, 2))
+plot(dvi1, col = cl, main = 'DV1 at time 1')
+plot(dvi2, col = cl, main = "DVI at time 2")
+
+#Vedere l'andamento e cosa è cambiato da defor1 a defor2
+difdvi <- dvi1 - dvi2 #differenza dei due momenti temporali
+#creo una nuova colorRampPalette
+cld <- colorRampPalette(c('blue', 'white', 'red')) (200)
+plot(difdvi, col = cld, main = 'Difference DVI1 and DV2')
+
+#CALCOLO NDVI1 e NDV2
+# (NIR - RED) / (NIR + RED)
+ndvi1 <- (defor1$defor1.1 - defor1$defor1.2) / (defor1$defor1.1 + defor1$defor1.2)
+ndvi2 <- (defor2$defor2.1 - defor2$defor2.2) / (defor2$defor2.1 + defor2$defor2.2)
+
+#plot con par
+par(mfrow = c(1, 2))
+plot(ndvi1, col = cl, main = 'NDVI at time 1')
+plot(ndvi2, col = cl, main = "NDVI at time 2")
+
+#Vedere l'andamento e cosa è cambiato da defor1 a defor2
+difndvi <- ndvi1 - ndvi2
+#plot
+plot(difndvi, col = cld, main = 'Difference between NDVIs') #mostra le aree con maggior perdita di vegetazione :( 
+
+#usiamo la funzione spectralindices() di RStoolbox...
+#come usare r from scratch, ossia da 0
+#per defor1
+vi1<- spectralIndices(defor1, green = 3, red = 2, nir = 1)
+plot(vi1, col = cl)
+#per defor2
+vi2<- spectralIndices(defor2, green = 3, red = 2, nir = 1)
+plot(vi2, col = cl)
+
+# ----- END -----
