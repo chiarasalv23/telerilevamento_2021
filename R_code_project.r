33ba95633f67077f99a210c92d404a27b1f90c25
ChiaraXXIII <80899048+chiarasalv23@users.noreply.github.com>
2021-06-18 19:43:52 +0200

    Update R_code_project.r

--- R_code_project.r
+++ R_code_project.r
@@ -6,48 +6,41 @@
 require(raster)
 require(RStoolbox)
 require(ggplot2) 
+require(gridExtra)
 
 # Working Directory 
-setwd("C:/lab/lake/")
+setwd("C:/lab/desert/")
 
 # Import dei dati (scaricati dall'earth observer della nasa)
-vik1 <- brick("lakevictoria1.jpg")
-vik2 <- brick("lakevictoria2.jpg")
+desert <- brick("namibia.jpg")
 
-# vediamo cosa vik1 e vik2 ci dicono...
-vik1 
+# visualizziamo informazioni relative al dato
+desert
 # class      : RasterBrick 
-# dimensions : 2634, 3758, 9898572, 3  (nrow, ncol, ncell, nlayers)
+# dimensions : 3618, 5427, 19634886, 3  (nrow, ncol, ncell, nlayers)
 # resolution : 1, 1  (x, y)
-# extent     : 0, 3758, 0, 2634  (xmin, xmax, ymin, ymax)
+# extent     : 0, 5427, 0, 3618  (xmin, xmax, ymin, ymax)
 # crs        : NA 
-# source     : C:/lab/lake/lakevictoria1.jpg 
-# names      : lakevictoria1.1, lakevictoria1.2, lakevictoria1.3 --> sono le bande
-# min values :               0,               0,               0 
-# max values :             255,             255,             255 
+# source     : C:/lab/desert/namibia.jpg 
+# names      : namibia.1, namibia.2, namibia.3  --> bande (rosse, verde, blue)
+# min values :         0,         0,         0 
+# max values :       255,       255,       255
 
-vik2
-# class      : RasterBrick 
-# dimensions : 2634, 3758, 9898572, 3  (nrow, ncol, ncell, nlayers)
-# resolution : 1, 1  (x, y)
-# extent     : 0, 3758, 0, 2634  (xmin, xmax, ymin, ymax)
-# crs        : NA 
-# source     : C:/lab/lake/lakevictoria2.jpg 
-# names      : lakevictoria2.1, lakevictoria2.2, lakevictoria2.3 --> sono le bande
-# min values :               0,               0,               0 
-#max values :             255,             255,             255
+# Plot RGB
+plotRGB(desert, r = 1, g = 2, b = 3, stretch = 'lin')
 
-# con ggplot creo un "par" più figo delle due immagini... 
-p1 <- ggRGB(vik1, r = 1, g = 2, b = 3, stretch = 'lin') + ggtitle("Lake Victoria May 8, 2013")
-p2 <- ggRGB(vik2, r = 1, g = 2, b = 3, stretch = 'lin') + ggtitle("Lake Victoria May 27, 2021")
-grid.arrange(p1, p2, ncol = 2) 
+# ricavo le firme spettrali...
+click(desert, id = T, xy = T, cell = T, type = 'p', pch = 16, cex = 4, col = 'green')
 
-# Adesso procediamo con la classificazione ? BOH? Non lo so ancora...
+# x      y     cell namibia.1 namibia.2 namibia.3
+# 1  611.5  812.5 15223347       225       154        72
+# 2 3995.5 2850.5  4166505       165       130        88
+# 3 1506.5 1218.5 13020880        79        58        29
 
-vik1_1 <- vik1$lakevictoria1.1
-vik2_1 <- vik2$lakevictoria2.1
-
-par(mfrow = c(1, 2))
-plot(vik1_1, col = cl, main = 'Lake Victoria 1 band 1') 
-plot(vik2_1, col = cl, main = 'Lake Victoria 2 band 1')
-
+# creo un dataframe con le percentuali di copertura...
+spectrals <- data.frame(bandz, dune, northarea, river)
+spectrals
+# bandz dune northarea river
+# 1     1  225       165    79
+# 2     2  154       130    58
+# 3     3   72        88    29
