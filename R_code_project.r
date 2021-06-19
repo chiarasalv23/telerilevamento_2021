54f00dbaaf83c88557cb3baa4def8c46fbf652ba
ChiaraXXIII <80899048+chiarasalv23@users.noreply.github.com>
2021-06-19 19:31:38 +0200

    Update R_code_project.r

--- R_code_project.r
+++ R_code_project.r
@@ -7,40 +7,53 @@ require(raster)
 require(RStoolbox)
 require(ggplot2) 
 require(gridExtra)
+require(lattice)
 
 # Working Directory 
-setwd("C:/lab/desert/")
+setwd("C:/lab/canada/")
 
-# Import dei dati (scaricati dall'earth observer della nasa)
-desert <- brick("namibia.jpg")
+list_2017 <- list.files(pattern = "2017")
+list_2020 <- list.files(pattern = '2020')
 
-# visualizziamo informazioni relative al dato
-desert
-# class      : RasterBrick 
-# dimensions : 3618, 5427, 19634886, 3  (nrow, ncol, ncell, nlayers)
-# resolution : 1, 1  (x, y)
-# extent     : 0, 5427, 0, 3618  (xmin, xmax, ymin, ymax)
-# crs        : NA 
-# source     : C:/lab/desert/namibia.jpg 
-# names      : namibia.1, namibia.2, namibia.3  --> bande (rosse, verde, blue)
-# min values :         0,         0,         0 
-# max values :       255,       255,       255
+import_2017 <- lapply(list_2017, raster)
+import_2020 <- lapply(list_2020, raster)
 
-# Plot RGB
-plotRGB(desert, r = 1, g = 2, b = 3, stretch = 'lin')
+stack_2017 <- stack(import_2017)
+stack_2020 <- stack(import_2020)
 
-# ricavo le firme spettrali...
-click(desert, id = T, xy = T, cell = T, type = 'p', pch = 16, cex = 4, col = 'green')
+# BANDE LANDSAT 8 
+# B2: Blue, B3: Green, B4: Red, B5: NIR
+# La B1 non mi serve in questo momento...
 
-# x      y     cell namibia.1 namibia.2 namibia.3
-# 1  611.5  812.5 15223347       225       154        72
-# 2 3995.5 2850.5  4166505       165       130        88
-# 3 1506.5 1218.5 13020880        79        58        29
+# Bande 2017
+nir_2017 <- stack_2017$X046017_2017_B5
+red_2017 <- stack_2017$X046017_2017_B4
+green_2017 <- stack_2017$X046017_2017_B3
+blue_2017 <- stack_2017$X046017_2017_B2
+# Bande 2020
+nir_2020 <- stack_2020$X046017_2020_B5
+red_2020 <- stack_2020$X046017_2020_B4
+green_2020 <- stack_2020$X046017_2020_B3
+blue_2020 <- stack_2020$X046017_2020_B2
 
-# creo un dataframe con le percentuali di copertura...
-spectrals <- data.frame(bandz, dune, northarea, river)
-spectrals
-# bandz dune northarea river
-# 1     1  225       165    79
-# 2     2  154       130    58
-# 3     3   72        88    29
+# NDVI 
+cl_ndvi <- colorRampPalette(c("lightskyblue", "orange", "yellow"))(300) # Palette per NDVI
+ndvi_2017 <- (nir_2017 - red_2017) / (nir_2017 + red_2017)
+ndvi_2020 <- (nir_2020 - red_2020) / (nir_2020 + red_2020)
+
+par(mfrow = c(1,2))
+plot(ndvi_2017, col = cl_ndvi, main = 'Great Slave Lake, 31/8/2017 NDVI')
+plot(ndvi_2020, col = cl_ndvi, main = 'Great Slave Lake, 20/6/2020 NDVI')
+
+# True color images
+# Creo degli stack con solo le bande VIS per ottenere delle immagini a colori naturali
+nci_2017 <- stack(red_2017, green_2017, blue_2017)
+nci_2020 <- stack(red_2020, green_2020, blue_2020)
+# plot 
+par(mfrow = c(1, 2))
+plotRGB(nci_2017, axes = TRUE, stretch = 'lin', main = 'Natural color Great Slave Lake 2017')
+plotRGB(nci_2020, axes = TRUE, stretch = 'lin', main = 'Natural color Great Slave Lake 2020')
+
+# Differenza tra i due NDVI
+diff_ndvi <- ndvi_2020 - ndvi_2017
+plot(diff_ndvi, col = cl_ndvi)
